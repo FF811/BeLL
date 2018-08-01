0b638a6cd8578d6063bcd603f06e4e088b81908a
Florens <example@example.com>
2018-08-01 10:45:55 +0200

    now really

--- main.cpp
+++ main.cpp
@@ -260,44 +260,6 @@ bool display_funktion()
 	glRotatef(sumxy, -sin((45-sumz)*pi/180), cos((45-sumz)*pi/180), 0);
 	drawfunction(fctsh, 2);
 	drawcoordinates(2);
-	/*if (d3d(fctsh)) 
-	{
-		while (x < 2)
-		{	
-			glBegin(GL_TRIANGLE_STRIP);
-			y = -2;
-			while (y < 2) {
-				glVertex3f(x, y, to_value(fctsh, x, y, true));
-				glVertex3f(x+0.4, y, to_value(fctsh, x+0.4, y, true));
-				y = y + 0.4;
-			}
-			x = x + 0.4;
-			glEnd();
-		}
-	}
-	else {
-		glBegin(GL_LINE_STRIP);
-		while (x < 2)
-		{
-			
-			glVertex3f(x, to_value(fctsh, x, y, false),0);
-			x = x + 0.05;
-			
-		}
-		glEnd();
-	}
-
-	{glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-	glLineWidth(2.0);
-	glBegin(GL_LINES);
-
-	glColor3f(0, 1, 0);
-	glVertex3f(-10, 0, 0); glVertex3f(10, 0, 0);
-	glColor3f(1, 0, 0);
-	glVertex3f(0, -10, 0); glVertex3f(0, 10, 0);
-	glColor3f(0, 0, 1);
-	glVertex3f(0, 0, -10); glVertex3f(0, 0, 10);
-	glEnd(); }*/
 
 	glPopMatrix();
 	glMatrixMode(GL_MODELVIEW);
